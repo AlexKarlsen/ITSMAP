40baa4f9ab4ae9476cf97e288577378fea7f1d94
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2017-05-19 11:15:32 +0200

    Implemented basic lobby activity

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/LobbyActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/LobbyActivity.java
@@ -2,6 +2,8 @@ package com.example.alex.pubgolf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LobbyActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        // Initialize the create game button.
+        Button createGameButton = (Button) findViewById(R.id.createGameButton);
+        createGameButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create intent for create game activity...
+            }
+        });
+
+        // Initialize the join game button.
+        Button joinGameButton = (Button) findViewById(R.id.joinGameButton);
+        joinGameButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create intent for join game activity...
+            }
+        });
     }
 }
