44ba5a3669278bbc637af3385e5ef5a354c79bc0
Alex Karlsen <alexkarlsen7@gmail.com>
2017-05-23 12:06:12 +0200

    Added Firebase helper + fixed manifest

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/GameService.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/GameService.java
@@ -3,15 +3,28 @@ package com.example.alex.pubgolf;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class GameService extends Service {
+
+    //Logging keys
+    private static final String LOG = "GameService";
+
+
     public GameService() {
     }
 
-    private DatabaseReference mDatabase;
-// ...
-    mDatabase = FirebaseDatabase.getInstance().getReference();
 
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(LOG, "Background service onCreate");
+
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
