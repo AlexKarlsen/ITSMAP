90a928b48d803127b555605e4e6308551c9db694
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2017-05-23 13:07:54 +0200

    Implemented proper date picker functionality

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/EditGameActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/EditGameActivity.java
@@ -1,15 +1,22 @@
 package com.example.alex.pubgolf;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.alex.pubgolf.Models.Game;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 
 public class EditGameActivity extends AppCompatActivity {
 
@@ -28,6 +35,7 @@ public class EditGameActivity extends AppCompatActivity {
         nameEditText = (EditText) this.findViewById(R.id.nameEditText);
         descriptionEditText = (EditText) this.findViewById(R.id.descriptionEditText);
         dateEditText = (EditText) this.findViewById(R.id.dateEditText);
+        setupDateEditText();
 
         // Initialize the host game button.
         Button continueButton = (Button) this.findViewById(R.id.continueButton);
@@ -37,6 +45,58 @@ public class EditGameActivity extends AppCompatActivity {
                 onContinueClicked();
             }
         });
+    }
+
+    protected void setupDateEditText() {
+
+        // Spawn a date picker when clicking the edit text.
+        dateEditText.setKeyListener(null);
+        dateEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+                if (!hasFocus) {
+                    return;
+                }
+
+                onDateEditTextInteraction(dateEditText);
+            }
+        });
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                onDateEditTextInteraction(dateEditText);
+            }
+        });
+    }
+
+    protected void onDateEditTextInteraction(final EditText editText) {
+
+        // Modified from: https://stackoverflow.com/questions/36662642/how-to-open-datepicker-dialog-on-click-of-edit-text-android
+
+        DatePickerDialog.OnDateSetListener dpd = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                int monthString = monthOfYear + 1;
+                String dateString = dayOfMonth+"/"+monthString+"/"+year;
+                editText.setText(dateString);
+            }
+        };
+
+        // Prepopulate the date picker with today's date.
+        Timestamp nowTimestamp = new Timestamp(System.currentTimeMillis());
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(nowTimestamp.getTime());
+
+        DatePickerDialog d = new DatePickerDialog(EditGameActivity.this, dpd, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        d.show();
     }
 
     protected void onContinueClicked() {
