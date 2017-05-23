43227c7daacbb792d3101e1cbc94f573f4942e04
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2017-05-23 11:37:51 +0200

    Solved cradle build issue

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/HostGameActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/HostGameActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.alex.pubgolf.Models.Game;
+
 public class HostGameActivity extends AppCompatActivity {
 
     EditText nameEditText;
@@ -35,8 +37,21 @@ public class HostGameActivity extends AppCompatActivity {
 
     void onContinueClicked() {
 
+        // Validate user input?
+
+        // Get the values input by the user.
         String name = nameEditText.getText().toString();
         String description = descriptionEditText.getText().toString();
         String date = dateEditText.getText().toString();
+
+        // Create the new game object.
+        Game game = new Game();
+        game.Title = name;
+        game.Description = description;
+        game.StartTime = (long) 0; // Get the actual time instead.
+        game.HoleIndex = (long) 0;
+        game.State = Game.GameState.Created;
+
+        // Pass the game object on to the course edit activity.
     }
 }
