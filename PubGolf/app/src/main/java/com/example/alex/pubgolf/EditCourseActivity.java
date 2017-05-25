cebf0dc665cfd17961f61341f11ec2ab85de1a88
Emil <emil-ny@hotmail.com>
2017-05-25 10:19:43 +0200

    HoleArrayAdapter

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/EditCourseActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/EditCourseActivity.java
@@ -9,14 +9,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
+import com.example.alex.pubgolf.Adapters.GameArrayAdapter;
+import com.example.alex.pubgolf.Adapters.HoleArrayAdapter;
 import com.example.alex.pubgolf.Models.Game;
+import com.example.alex.pubgolf.Models.Hole;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditCourseActivity extends AppCompatActivity {
 
     Game game;
     GameService gameService;
     Boolean bound = false;
+
+    ArrayList<Hole> holesList;
+
+    // Views
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,7 @@ public class EditCourseActivity extends AppCompatActivity {
         startService(gameServiceIntent);
         bindService(gameServiceIntent, connection, Context.BIND_IMPORTANT);
 
+        listView = (ListView) findViewById(R.id.holeListView);
         Button doneButton = (Button) findViewById(R.id.doneButton);
 
         // Add on click handler
@@ -42,18 +55,29 @@ public class EditCourseActivity extends AppCompatActivity {
                 gameService.createNewGame(game);
             }
         });
+
+        updateListView(holesList);
     }
 
     protected void handleStartWithIntent(Intent intent) {
         game = (Game) intent.getExtras().getSerializable(EditGameActivity.EXTRA_GAME);
+        //holesList = game.
+    }
+
+    private void updateListView(ArrayList<Hole> data) {
+
+        if (data == null) { return; }
+
+        // Populate the list view with the weather info using a custom adapter.
+        HoleArrayAdapter adapter = new HoleArrayAdapter(this, R.layout.hole_info_list_item, data);
+        listView.setAdapter(adapter);
     }
 
     // Modified from: https://developer.android.com/guide/components/bound-services.html#Binder
     private ServiceConnection connection = new ServiceConnection() {
 
         @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
+        public void onServiceConnected(ComponentName className, IBinder service) {
 
             // Cast the IBinder and get WeatherInfoService instance.
             GameService.GameServiceBinder binder = (GameService.GameServiceBinder) service;
