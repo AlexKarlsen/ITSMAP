614b41cd5d9fd877b1387f3edc037fc74331f59c
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2017-05-25 10:24:03 +0200

    Added base implementation of edit hole activity

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/EditHoleActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/EditHoleActivity.java
@@ -1,13 +1,131 @@
 package com.example.alex.pubgolf;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.alex.pubgolf.Models.Game;
+import com.example.alex.pubgolf.Models.Hole;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
 
 public class EditHoleActivity extends AppCompatActivity {
+
+    EditText nameEditText;
+    EditText descriptionEditText;
+    EditText dateEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_hole);
+
+        // Initialize the subviews.
+        nameEditText = (EditText) this.findViewById(R.id.nameEditText);
+        descriptionEditText = (EditText) this.findViewById(R.id.descriptionEditText);
+        dateEditText = (EditText) this.findViewById(R.id.dateEditText);
+        setupDateEditText();
+
+        // Initialize the host game button.
+        Button doneButton = (Button) this.findViewById(R.id.doneButton);
+        doneButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDoneClicked();
+            }
+        });
+    }
+
+    protected void setupDateEditText() {
+
+        // Spawn a date picker when clicking the edit text.
+        dateEditText.setKeyListener(null);
+        dateEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+                if (!hasFocus) {
+                    return;
+                }
+
+                onDateEditTextInteraction(dateEditText);
+            }
+        });
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                onDateEditTextInteraction(dateEditText);
+            }
+        });
+    }
+
+    protected void onDateEditTextInteraction(final EditText editText) {
+
+        // Modified from: https://stackoverflow.com/questions/36662642/how-to-open-datepicker-dialog-on-click-of-edit-text-android
+
+        DatePickerDialog.OnDateSetListener dpd = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                int monthString = monthOfYear + 1;
+                String dateString = dayOfMonth+"/"+monthString+"/"+year;
+                editText.setText(dateString);
+            }
+        };
+
+        // Prepopulate the date picker with today's date.
+        Timestamp nowTimestamp = new Timestamp(System.currentTimeMillis());
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(nowTimestamp.getTime());
+
+        DatePickerDialog d = new DatePickerDialog(EditHoleActivity.this, dpd, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        d.show();
+    }
+
+    protected void onDoneClicked() {
+
+        // Validate user input.
+        if (!inputIsValid()) {
+
+            // Display a toast to the user.
+            Context context = getApplicationContext();
+            String text = "Input invalid!"; // TODO: Localize text.
+            Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+            toast.show();
+
+            return;
+        }
+
+        // Get the values input by the user.
+        String name = nameEditText.getText().toString();
+        String description = descriptionEditText.getText().toString();
+        String date = dateEditText.getText().toString();
+
+        // Create the new hole object.
+        Hole hole = new Hole();
+        hole.Name = name;
+        hole.Description = description;
+
+        // Return from activity with the hole.
+    }
+
+    protected boolean inputIsValid() {
+
+        // TODO: Actually validate the input.
+        return true;
     }
 }
