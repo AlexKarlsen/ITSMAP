3986a8e9d0007a1221171aab6f7d67856bf79851
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2017-05-29 14:12:59 +0200

    Added tab bar navigation activity

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/GameListActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/GameListActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.alex.pubgolf.Adapters.GameArrayAdapter;
