44d0f8ca500a9e213739677ef0f7c54de3e9a263
Emil <emil-ny@hotmail.com>
2017-05-30 14:19:12 +0200

    GameNavigation activity starts HoleDetailActivity which starts add score

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/HoleDetailActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/HoleDetailActivity.java
@@ -1,8 +1,11 @@
 package com.example.alex.pubgolf;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.alex.pubgolf.Models.Game;
@@ -10,12 +13,21 @@ import com.example.alex.pubgolf.Models.Hole;
 
 import java.util.ArrayList;
 
+import static java.security.AccessController.getContext;
+
 public class HoleDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_HOLE = "EXTRA_HOLE";
+    public static final String EXTRA_GAME = "EXTRA_GAME";
+
+    Game game;
+    Hole hole;
 
     TextView nameTextView;
     TextView descriptionTextView;
     TextView timeTextView;
     TextView indexTextView;
+    Button addScorebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +38,37 @@ public class HoleDetailActivity extends AppCompatActivity {
         descriptionTextView = (TextView) findViewById(R.id.holeDescriptionTextView);
         timeTextView = (TextView) findViewById(R.id.holeTimeTextView);
         indexTextView = (TextView) findViewById(R.id.holeIndexTextView);
+        addScorebutton = (Button) findViewById(R.id.addScoreButton);
 
         Intent intent = getIntent();
         if (intent != null) {
             handleStartWithIntent(intent);
         }
+
+        addScorebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Pass the selected hole index and game to the score activity.
+                Context context = getApplicationContext();
+                Intent addScoreIntent = new Intent(context, AddScoreActivity.class);
+                addScoreIntent.putExtra(EXTRA_GAME, game);
+                addScoreIntent.putExtra(EXTRA_HOLE, hole);
+                startActivity(addScoreIntent);
+            }
+        });
     }
 
     protected void handleStartWithIntent(Intent intent) {
-        //Hole hole = (Hole) intent.getExtras().getSerializable();
-        //if (hole == null) return;
-        //updateFields(hole);
+        Hole holeFromIntent = (Hole) intent.getExtras().getSerializable(GameNavigationActivity.EXTRA_HOLE);
+        Game gameFromIntent = (Game) intent.getExtras().getSerializable(GameNavigationActivity.EXTRA_GAME);
+        hole = holeFromIntent;
+        game = gameFromIntent;
+
+        if (hole == null) return;
+        updateFields();
     }
 
-    private void updateFields(Hole hole)
+    private void updateFields()
     {
         nameTextView.setText(hole.Name);
         descriptionTextView.setText(hole.Description);
