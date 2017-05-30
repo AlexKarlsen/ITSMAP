5eb3de060925fe6b0b45bfb58dcf216c1b96ca8f
Emil <emil-ny@hotmail.com>
2017-05-30 17:37:33 +0200

    fixd detail not being syncd after add score

--- PubGolf/app/src/main/java/com/example/alex/pubgolf/HoleDetailActivity.java
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/HoleDetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.alex.pubgolf;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -60,6 +63,11 @@ public class HoleDetailActivity extends AppCompatActivity {
         // If hole isnt active or passed then disable button.
         if (hole.Index > game.HoleIndex)
             addScorebutton.setEnabled(false);
+
+        // Register broadcast receivers.
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(GameService.BROADCAST_GAME_SERVICE_RESULT);
+        LocalBroadcastManager.getInstance(this).registerReceiver(onGameServiceResult, filter);
     }
 
     protected void handleStartWithIntent(Intent intent) {
@@ -81,4 +89,44 @@ public class HoleDetailActivity extends AppCompatActivity {
         if (hole.Time != 0)
             timeTextView.setText(hole.GetTimeAsTimestamp().toString());
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // Unregister broadcast receivers.
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(onGameServiceResult);
+    }
+
+    private BroadcastReceiver onGameServiceResult = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String description = intent.getStringExtra(GameService.EXTRA_DESCRIPTION);
+
+            // Only react on game change notifications.
+
+            if (description.equals(GameService.OLD_GAME_CHANGED)) {
+
+                Game receivedGame = (Game) intent.getSerializableExtra(GameService.EXTRA_GAME);
+
+                // Only update if the updated game is the one currently displayed.
+                if (game != null && receivedGame != null) {
+                    if (game.Key.equals(receivedGame.Key)) {
+                        // Finds the updated version of the Hole displayed
+                        for (Hole holeIterator: receivedGame.Holes) {
+                            if (holeIterator.Name.equals(hole.Name))
+                            {
+                                hole = holeIterator;
+                                break;
+                            }
+                        }
+
+                        game = receivedGame;
+                        updateFields();
+                    }
+                }
+            }
+        }
+    };
 }
