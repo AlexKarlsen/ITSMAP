4f1ce6dd68b40443bf4ef2565f31d1bc337e8403
Alex Karlsen <alexkarlsen7@gmail.com>
2017-05-30 10:45:43 +0200

    AddScoreActivity added

---
+++ PubGolf/app/src/main/java/com/example/alex/pubgolf/AddScoreActivity.java
@@ -0,0 +1,21 @@
+package com.example.alex.pubgolf;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_score);
+    }
+
+    //Spinner scoreSpinner = (Spinner) findViewById(R.id.scoreSpinner);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+    //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, )
+    // Specify the layout to use when the list of choices appears
+    //adapter.(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+    //scoreSpinner.(adapter);
+}
